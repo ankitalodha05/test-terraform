0f5a7969c571ef8e0137b2982fe6ca6089c8cf84
ankitalodha05 <aankitalodha05@gmail.com>
2025-04-01 15:35:11 +0530

    comment added

--- practice/day-1/main.tf
+++ practice/day-1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dev" {
-  instance_type = var.instance_type
+  instance_type = var.instance_type #inserting value from variables
   key_name      = var.key_name
   ami           = var.ami
 
